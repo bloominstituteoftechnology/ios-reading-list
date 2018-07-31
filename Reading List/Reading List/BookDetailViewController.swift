4980c5169fe4e196f011a2a071752d5819c92c6a
Conner Alegre <cga0@pm.me>
2018-07-31 13:38:40 -0700

    Complete Storyboard interfaces

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Conner on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func saveBookUpdate(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet var bookDescriptionText: UITextView!
+    @IBOutlet var bookTitleText: UITextField!
+}
