3f5b858c807ef1c4b8e31de3e3f842cff526d938
Linh Bouniol <paulalinh89@gmail.com>
2018-07-31 13:47:51 -0700

    Created storyboards

---
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Linh Bouniol on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var textView: UITextView!
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Linh Bouniol on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet var bookLabel: UILabel!
+    @IBOutlet var checkboxButtonLabel: UIButton!
+    
+    @IBAction func checkboxButton(_ sender: Any) {
+    }
+    
+}
