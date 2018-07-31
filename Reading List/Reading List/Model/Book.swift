79bb0849e3ef2b17c70babc9ae8122fa0cc22282
Carolyn Lea <carolynrlea@gmail.com>
2018-07-31 13:54:55 -0600

    Completed Book model using Struct:Equatable, Codable

--- Reading List/Reading List/Model/Book.swift
+++ Reading List/Reading List/Model/Book.swift
@@ -8,7 +8,16 @@
 
 import Foundation
 
-struct Book
+struct Book: Equatable, Codable
 {
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: Bool
     
+    init(title: String, reasonToRead: String, hasBeenRead: Bool = false)
+    {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
 }
