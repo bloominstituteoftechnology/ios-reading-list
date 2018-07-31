7bd69fb1a50f574bbe8e5aeeb269031a5f9ebe2d
Linh Bouniol <paulalinh89@gmail.com>
2018-07-31 15:41:53 -0700

    Set up detail view controller

--- Reading List/Reading List/View Controllers/BookDetailViewController.swift
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class BookDetailViewController: UIViewController {
+
+    var bookController: BookController?
     
+    var book: Book?
+
     @IBOutlet var textField: UITextField!
     @IBOutlet var textView: UITextView!
     
     @IBAction func save(_ sender: Any) {
+        guard let title = textField.text, let reasonToRead = textView.text else { return }
+        
+        if book == nil {
+            bookController?.createBook(withTitle: title, reasonToRead: reasonToRead, hasBeenRead: false)
+        } else {
+            updateViews()
+        }
     }
     
 
@@ -22,15 +33,15 @@ class BookDetailViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func updateViews() {
+        if let book = book {
+            textField.text = book.title
+            textView.text = book.reasonToRead
+            navigationController?.title = book.title
+        } else {
+            navigationController?.title = "Add a new book"
+        }
     }
-    */
 
 }
