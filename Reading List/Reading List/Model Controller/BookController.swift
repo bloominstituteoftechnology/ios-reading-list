e2a80b4bb63dd3ef44fd004e7ed782ff555e7ef6
stdemicheli <stdemicheli@gmail.com>
2018-07-31 21:48:16 +0200

    Implemented CRUD functionality in BookController. Also added computed properties returning read and unread books.

--- Reading List/Reading List/Model Controller/BookController.swift
+++ Reading List/Reading List/Model Controller/BookController.swift
@@ -10,6 +10,40 @@ import Foundation
 
 class BookController {
     
+    func create(title: String, reasonToRead: String) {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        books.append(book)
+        saveToPersistentStore()
+    }
+    
+    func delete(book: Book) {
+        if let index = books.index(of: book) {
+            books.remove(at: index)
+        }
+        saveToPersistentStore()
+    }
+    
+    func updateHasBeenRead(for book: Book) {
+        if let index = books.index(of: book) {
+            var book = books[index]
+            book.hasBeenRead = !book.hasBeenRead
+            books.remove(at: index)
+            books.insert(book, at: index)
+        }
+        saveToPersistentStore()
+    }
+    
+    func updateBook(for book: Book, title: String, reasonToRead: String) {
+        if let index = books.index(of: book) {
+            var book = books[index]
+            book.title = title
+            book.reasonToRead = reasonToRead
+            books.remove(at: index)
+            books.insert(book, at: index)
+        }
+        saveToPersistentStore()
+    }
+    
     func saveToPersistentStore() {
         do {
             // Encoding entire books array to PropertyList and write it to "disk" at readingListURL
@@ -42,6 +76,16 @@ class BookController {
         let fileName = "ReadingList.plist"
         guard let documentsDir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
         return documentsDir.appendingPathComponent(fileName)
+    }
+    
+    var readBooks: [Book] {
+        // Return all books that have been read
+        return books.filter( { $0.hasBeenRead == true } )
+    }
+    
+    var unreadBooks: [Book] {
+        // Return all books that have not yet been read
+        return books.filter( { $0.hasBeenRead == false } )
     }
     
     private(set) var books = [Book]()
