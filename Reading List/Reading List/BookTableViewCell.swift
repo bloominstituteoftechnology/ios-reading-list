393964961a38e7cf834c6ed573a8c487d60fb153
Conner Alegre <cga0@pm.me>
2018-07-31 13:13:16 -0700

    Add storyboards with base views including constrains. Added segues for adding and updating books.

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Conner on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Reading List/Reading List/ReadingListTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ReadingListTableViewController.swift
+//  Reading List
+//
+//  Created by Conner on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ReadingListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath)
+
+
+        return cell
+    }
+}
