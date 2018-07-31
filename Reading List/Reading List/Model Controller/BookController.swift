ae6188660f20c921990bf8038302b1d9378b1823
Linh Bouniol <paulalinh89@gmail.com>
2018-07-31 12:24:44 -0700

    Created a struct model to hold book properties

---
+++ Reading List/Reading List/Model Controller/BookController.swift
@@ -0,0 +1,15 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Linh Bouniol on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    
+   
+    
+}
