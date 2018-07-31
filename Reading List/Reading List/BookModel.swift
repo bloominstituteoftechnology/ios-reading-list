d6b45e949839304d82bcc2c495a6fbcd8672a2d4
William Bundy <williambundy@icloud.com>
2018-07-31 13:27:23 -0700

    Project basics: storyboard and model

---
+++ Reading List/Reading List/BookModel.swift
@@ -0,0 +1,85 @@
+//
+//  BookModel.swift
+//  Reading List
+//
+//  Created by William Bundy on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Comparable, Codable
+{
+	var name:String
+	var readingReason:String
+	var read:Bool
+
+	static func <(lhs:Book, rhs:Book) -> Bool
+	{
+		if lhs.read || rhs.read {
+			if lhs.read && rhs.read {
+				return lhs.name < rhs.name
+			}
+
+			return lhs.read
+		}
+
+		return lhs.name < rhs.name
+	}
+}
+
+class BookController
+{
+	var books:[Book] = []
+
+	var unreadBooks:[Book] = []
+	var readBooks:[Book] = []
+	
+
+	func add(_ book:Book)
+	{
+		books.append(book)
+	}
+
+	@discardableResult
+	func create(_ name:String, _ reason:String) -> Book
+	{
+		let b = Book(name:name, readingReason:reason, read:false)
+		add(b)
+		return b
+	}
+
+	func getDefaultPersistURL() -> URL
+	{
+		return FileManager.default.urls(for: .applicationSupportDirectory, in: .userDomainMask)[0].appendingPathComponent("Books.plist")
+	}
+
+	func save()
+	{
+		do {
+			let enc = PropertyListEncoder()
+			let data = try enc.encode(books)
+			try data.write(to: getDefaultPersistURL())
+		} catch {
+			print("Failed to write to default persist URL: ", getDefaultPersistURL())
+		}
+	}
+
+	func load()
+	{
+		let url = getDefaultPersistURL()
+		let fm = FileManager.default
+		if !fm.fileExists(atPath: url.path) {
+			return
+		}
+
+		do {
+			let data = try Data(contentsOf: getDefaultPersistURL())
+			let dec = PropertyListDecoder()
+			books = try dec.decode([Book].self, from: data)
+		} catch {
+			print("Failed to load from default persist URL: ", url)
+		}
+	}
+
+}
---
+++ Reading List/Reading List/BookView.swift
@@ -0,0 +1,9 @@
+//
+//  BookView.swift
+//  Reading List
+//
+//  Created by William Bundy on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
