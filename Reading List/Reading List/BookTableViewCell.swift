abf5952f0bda6db50cb6e60875ad30342ab57877
Farhan Farooqui <farhan@farooqui.net>
2018-09-04 19:22:14 -0400

    cell ready

--- Reading List/Reading List/BookTableViewCell.swift
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -8,25 +8,32 @@
 
 import UIKit
 
-class BookTableViewCell: UITableViewCell {
+class BookTableViewCell: UITableViewCell{
+    
+    var book: Book?
+    weak var delegate: BookTableViewCellDelegate?
 
     @IBOutlet weak var bookLabel: UILabel!
 
     @IBOutlet weak var checkbutton: UIButton!
     
     @IBAction func checkMark(_ sender: Any) {
-        
+        delegate?.toggleHasBeenRead(for: self)
     }
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func updateViews(){
+        
+        bookLabel.text = book?.title
+        let checkImage = UIImage(named: "checked")
+        let uncheckedImage = UIImage(named: "unchecked")
+        if (book?.hasBeenRead == true){
+            checkbutton.setBackgroundImage(checkImage, for: .normal)
+        } else if (book?.hasBeenRead == false) {
+            checkbutton.setBackgroundImage(uncheckedImage, for: .normal)
+        }
+        
+        
+        
     }
 
 }
---
+++ Reading List/Reading List/BookTableViewCellDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  BookTableViewCellDelegate.swift
+//  Reading List
+//
+//  Created by Farhan on 9/4/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol BookTableViewCellDelegate: class {
+    
+    func toggleHasBeenRead(for cell: BookTableViewCell)
+
+    
+}
