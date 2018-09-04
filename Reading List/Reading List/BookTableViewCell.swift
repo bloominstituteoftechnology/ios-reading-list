ae481d48d67f130b32de6ecc4c00d2c7710846f1
Jason Modisett <jason@Jasons-MacBook-Pro.local>
2018-09-04 18:05:33 -0500

    oops i did terrible committing today. But now i'm commiting the final project  :O

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,35 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Jason Modisett on 9/4/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+protocol BookTableViewCellDelegate: class {
+    func toggleHasBeenRead(for cell: BookTableViewCell)
+}
+
+class BookTableViewCell: UITableViewCell {
+
+    private func updateViews() {
+        guard let book = book else { return }
+        
+        bookTitleLabel.text = book.title
+        hasBeenReadButton.setImage(book.hasBeenRead ? #imageLiteral(resourceName: "checked.png") : #imageLiteral(resourceName: "unchecked.png"), for: .normal)
+    }
+    
+    @IBAction func hasBeenReadButtonPressed(_ sender: Any) {
+        delegate?.toggleHasBeenRead(for: self)
+    }
+    
+    weak var delegate: BookTableViewCellDelegate?
+    
+    var book: Book? { didSet { updateViews() }}
+    
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var hasBeenReadButton: UIButton!
+    
+}
