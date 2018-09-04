38ee7322ef16f2499664d89e7b6188d15b112af2
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-04 13:28:23 -0600

    Add book model and model controller

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,94 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Dillon McElhinney on 9/4/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    
+    private(set) var books: [Book] = []
+    
+    var readBooks: [Book] {
+        return books.filter({ (book) -> Bool in
+            return book.hasBeenRead
+        })
+    }
+    
+    var unreadBooks: [Book] {
+        return books.filter({ (book) -> Bool in
+            return !book.hasBeenRead
+        })
+    }
+    
+    // MARK: CRUD Methods
+    //Create method
+    func createBook(title: String, reasonToRead: String) {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        
+        books.append(book)
+        saveToPersistentStore()
+    }
+    
+    //Update methods
+    func updateHasBeenRead(for book: Book) {
+        guard let index = books.index(of: book) else { return }
+        
+        books[index].hasBeenRead = !books[index].hasBeenRead
+        saveToPersistentStore()
+    }
+    
+    func update(_ book: Book, title: String, reasonToRead: String) {
+        guard let index = books.index(of: book) else { return }
+        
+        books[index].title = title
+        books[index].reasonToRead = reasonToRead
+        saveToPersistentStore()
+    }
+    
+    //Delete method
+    func delete(_ book: Book) {
+        guard let index = books.index(of: book) else { return }
+        
+        books.remove(at: index)
+        saveToPersistentStore()
+    }
+    
+    // MARK: - Persistence
+    //Method to encode books array and write it to disk
+    private func saveToPersistentStore() {
+        let pListEncoder = PropertyListEncoder()
+        guard let url = readingListURL else { return }
+        
+        do {
+            let booksData = try pListEncoder.encode(books)
+            try booksData.write(to: url)
+        } catch {
+            NSLog("Error encoding book data: \(error)")
+        }
+    }
+    
+    //Method to retrieve books data from disk and decode it
+    private func loadFromPersistentStore() {
+        do {
+            guard let url = readingListURL, FileManager.default.fileExists(atPath: url.path) else { return }
+            let booksData = try Data(contentsOf: url)
+            let plistDecoder = PropertyListDecoder()
+            let decodedBooks = try plistDecoder.decode([Book].self, from: booksData)
+            books = decodedBooks
+        } catch {
+            NSLog("Error decoding book data: \(error)")
+        }
+    }
+    
+    //Computed property containing the url for the reading list plist
+    var readingListURL: URL? {
+        guard let documentsURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        let fileName = "ReadingList.plist"
+        
+        return documentsURL.appendingPathComponent(fileName)
+    }
+}
