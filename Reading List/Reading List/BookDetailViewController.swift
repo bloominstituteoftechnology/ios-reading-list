e1fdb65ca5cef07bd3b850b44777ffc3ce297615
Dillon McElhinney <dillon.mce@gmail.com>
2018-09-04 15:43:40 -0600

    Add image to book model and ability to add image on book detail view

--- Reading List/Reading List/BookDetailViewController.swift
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BookDetailViewController: UIViewController {
+class BookDetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: - Properties
     var bookController: BookController?
@@ -16,6 +16,7 @@ class BookDetailViewController: UIViewController {
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var reasonToReadTextField: UITextView!
+    @IBOutlet weak var imageView: UIImageView!
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
@@ -25,21 +26,49 @@ class BookDetailViewController: UIViewController {
     }
 
     // MARK: - UI Methods
+    @IBAction func addImage(_ sender: UIButton) {
+        let imagePicker = UIImagePickerController()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            imagePicker.sourceType = .photoLibrary
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func saveBook(_ sender: UIBarButtonItem) {
         //Unwrap the text fields and views and make sure the required ones have content.
         guard let title = titleTextField.text, !title.isEmpty,
             let reasonToRead = reasonToReadTextField.text else { return }
         
+        //If there is an image picked, represent it as Data, else pass nil
+        let imageData: Data?
+        if let image = imageView.image {
+            imageData = UIImagePNGRepresentation(image)
+        } else {
+            imageData = nil
+        }
+        
         if let book = book {
             //Update the current book
-            bookController?.update(book, title: title, reasonToRead: reasonToRead, imageData: nil)
+            bookController?.update(book, title: title, reasonToRead: reasonToRead, imageData: imageData)
         } else {
             //Create a new book
-            bookController?.createBook(title: title, reasonToRead: reasonToRead, imageData: nil)
+            bookController?.createBook(title: title, reasonToRead: reasonToRead, imageData: imageData)
         }
         
         //Pop the view off of the stack
         navigationController?.popViewController(animated: true)
+    }
+    
+    // MARK: - Image picker controller delegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
+        
+        imageView.image = image
     }
     
     // MARK: - Private Utility Methods
@@ -54,6 +83,10 @@ class BookDetailViewController: UIViewController {
         title = book.title
         titleTextField.text = book.title
         reasonToReadTextField.text = book.reasonToRead
+        if let imageData = book.imageData {
+            imageView.image = UIImage(data: imageData)
+        }
+        
     }
     
 }
