6f380898c5c0960269bd9824d80438d1d4a91cd0
Iyin Raphael <iyinraphael@gmail.com>
2018-09-11 16:01:17 -0700

    added logic tableViewController

--- Reading List/Reading List/View Controller/ReadingListTableViewController.swift
+++ Reading List/Reading List/View Controller/ReadingListTableViewController.swift
@@ -8,8 +8,14 @@
 
 import UIKit
 
-class ReadingListTableViewController: UITableViewController {
-
+class ReadingListTableViewController: UITableViewController, BookTableViewCellDelegate {
+    func toggleHasBeenRead(for cell: BookTableViewCell) {
+        guard let index = tableView.indexPath(for: cell) else {return}
+        let book = self.bookFor(indexPath: index)
+        bookController.updateHasBeenRead(for: book)
+    }
+    
+    weak var delegate: BookTableViewCellDelegate?
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
         tableView.reloadData()
@@ -21,67 +27,59 @@ class ReadingListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        if section == 0 {
+            return bookController.readBooks.count
+        }else{
+            return bookController.unreadBooks.count
+        }
     }
 
-    /*
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell", for: indexPath) as? BookTableViewCell else {return UITableViewCell()}
+       let book = self.bookFor(indexPath: indexPath)
+        cell.delegate = self
         return cell
     }
-    */
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
+  
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            let book = self.bookFor(indexPath: indexPath)
+            bookController.delete(book: book)
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        if section == 0 {
+            return "Read Books"
+        }else{
+            return "Unread Books"
+        }
     }
-    */
+    
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
     }
-    */
+    
+    func bookFor(indexPath: IndexPath) -> Book{
+        if indexPath.section == 0 {
+            return bookController.readBooks[indexPath.row]
+        }else{
+            return bookController.unreadBooks[indexPath.row]
+        }
+    }
+    
+    let bookController = BookController()
 
 }
