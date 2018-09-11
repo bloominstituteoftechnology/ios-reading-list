3704fb21093cb665e9135adcc2d55e79f296bdba
Iyin Raphael <iyinraphael@gmail.com>
2018-09-11 13:20:43 -0700

    controller set up

--- Reading List/Reading List/Model Controller/BookController.swift
+++ Reading List/Reading List/Model Controller/BookController.swift
@@ -10,6 +10,26 @@ import Foundation
 
 class BookController {
    
+    func createBook(title: String, reasonToRead: String){
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        books.append(book)
+        saveToPersistence()
+    }
+    
+    func updateBook(book: Book, title: String, reasonToRead: String){
+        guard let index = books.index(of: book) else {return}
+        var scratch = book
+        scratch.reasonToRead = reasonToRead
+        scratch.title = title
+        books.remove(at: index)
+        books.insert(scratch, at: index)
+        saveToPersistence()
+    }
+    
+    func updateHasBeenRead(for book: Book) -> Bool{
+       return book.hasBeenRead != book.hasBeenRead
+    }
+    
     var books: [Book] = []
      
 }
