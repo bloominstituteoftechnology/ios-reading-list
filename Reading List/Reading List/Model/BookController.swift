76c3677649682a871e2722236b14d6e05b0de34b
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-12 02:41:57 -0700

    created persistence functions

--- Reading List/Reading List/Model/BookController.swift
+++ Reading List/Reading List/Model/BookController.swift
@@ -10,15 +10,69 @@ import Foundation
 
 class BookController {
     
-    // CRUD
-    // Reading
-    private(set) var books:[String] = []
+    // MARK: - CRUD
     
-    //creatiing the file name
+    // Reading
+    private(set) var books:[Book] = []
+    
+    // creating the file name
+    // creating computed property
     private var readingListURL: URL? {
         let fm = FileManager.default
         guard let directory = fm.urls(for: .documentDirectory, in: .userDomainMask).first else {return nil}
         return directory.appendingPathComponent("ReadingList.plist")
+    }
+    
+    // create new book
+    func createBook(withName title:String, reasonToRead:String, hasBeenRead:Bool) {
+        
+    }
+    
+    // MARK: - Persistence Functions
+    
+    
+    // this is where we will save the document
+    // will have to use an encoder --> do catch (for errors)
+    func saveToPersistentStore(){
+        
+        // create the url
+        guard let url = readingListURL else {return}
+        
+        do { // create encoder, encode the data, write data to specific file
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(books)
+            try data.write(to: url)
+        } catch  {
+            // store the errors in the users' log
+            NSLog("Error saving books to data: \(error)")
+        }
+    }
+    
+    
+    // this is where we will retrieve the document
+    // will have to use a decoder --> do catch (for errors)
+    
+    func loadFromPersistentStore(){
+        
+        // create the file manager
+        let fm = FileManager.default
+        
+        // create the url
+        guard let url = readingListURL, fm.fileExists(atPath: url.path) else {return}
+        
+        do {
+            // create the decoder
+            let decoder = PropertyListDecoder()
+            
+            // reference the data that was stored above
+            let data = try Data(contentsOf: url)
+            
+            // start to decode
+            books = try decoder.decode([Book].self, from: data)
+            
+        } catch  {
+            NSLog("Error loading books data: \(error)")
+        }
     }
     
 }
