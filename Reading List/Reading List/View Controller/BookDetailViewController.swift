0e1b9a0395e8d09e3879d2291b65446b6fd79676
jWarrenDev <Jerrick.Warren@gmail.com>
2018-09-12 20:22:24 -0700

    Finished RLTVC

--- Reading List/Reading List/View Controller/BookDetailViewController.swift
+++ Reading List/Reading List/View Controller/BookDetailViewController.swift
@@ -19,7 +19,7 @@ class BookDetailViewController: UIViewController {
         
     }
     
-    
+    var bookController: BookController?
     
     
     
--- Reading List/Reading List/View Controller/ReadingListTableViewController.swift
+++ Reading List/Reading List/View Controller/ReadingListTableViewController.swift
@@ -10,15 +10,28 @@ import UIKit
 
 class ReadingListTableViewController: UITableViewController, BookTableViewCellDelegate {
     
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    var bookController = BookController()
+    
+    
     func toggleHasBeenRead(for cell: BookTableViewCell) {
         guard let indexPath = tableView.indexPath(for: cell) else {return}
         let book = bookFor(indexPath: indexPath)
-        
         bookController.updateHasBeenRead(book: book)
         tableView.reloadData()
     }
     
-
+    
     let bookController = BookController()
     
     func bookFor(indexPath: IndexPath) -> Book {
@@ -29,95 +42,101 @@ class ReadingListTableViewController: UITableViewController, BookTableViewCellDe
             
             return bookController.unreadBooks[indexPath.row]
         }
-
-    
-        func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-        func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    
-    
-    
-    // MARK: - Table view data source
-
-        func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 2
-    }
-
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // remember we have to have two sections now
-        // use a conditional statement - Switch
         
-        switch section {
-        case 0:
-            return bookController.readBooks.count
-        case 1:
-            return bookController.unreadBooks.count
-        default:
-            return bookController.unreadBooks.count
+        
+  
+        
+        
+        
+    // MARK: - Table view data source
+        
+    // set a title for the books you want have read and the ones you have not read so far.
+        
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+            if section == 0 {
+                if bookController.readBooks.count == 0 {
+                    return nil
+                } else {
+                    return "Book's I've Read"
+                }
+            } else {
+                if bookController.unreadBooks.count == 0 {
+                    return nil
+                } else {
+                    return "Books I still need to read"
+                }
+            }
         }
-    }
-
+        
+    // set number of section.  In this case 2
+    func numberOfSections(in tableView: UITableView) -> Int {
+            // #warning Incomplete implementation, return the number of sections
+            return 2
+        }
     
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    // for this one because we have more than 1 section. Use a switch / case should be cleaner.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // remember we have to have two sections now
+            // use a conditional statement - Switch
+        
+            switch section {
+            case 0:
+                return bookController.readBooks.count
+            case 1:
+                return bookController.unreadBooks.count
+            default:
+                return bookController.unreadBooks.count
+            }
+        }
+        
+        // add the delegate between the custom cell and the TVC
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as? BookTableViewCell else {return UITableViewCell()}
+            
+            let book = bookFor(indexPath: indexPath)
+            
+            cell.book = book
+            cell.delegate = self
+        
+            // Configure the cell...
+        
+            return cell
+        }
+        
+        // enable the delete option
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                let book = bookFor(indexPath: indexPath)
+                
+                bookController.deleteBook(book: book)
+                tableView.deleteRows(at: [indexPath], with: .automatic)
+                
+            }
+        }
 
-        // Configure the cell...
-
-        return cell
+         // MARK: - Navigation
+         
+        // this is the segueway set up
+        
+        func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+            if segue.identifier == "AddBook" {
+                
+                guard let  destinationVC = segue.destination as? BookDetailViewController else {return}
+                
+                destinationVC.bookController = bookController
+                
+            } else if segue.identifier == "ShowBook" {
+                
+                guard let destinationVC = segue.destination as? BookDetailViewController else {return}
+                
+                let book = bookFor(indexPath: indexPath)
+                
+                destinationVC.bookController = bookController
+                
+                destinationVC.book = book
+            }
+         }
+        
+        
     }
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
 }
