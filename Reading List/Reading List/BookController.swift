91a64985fab8a1613c892638bb1602167ca5eb72
Stuart Sippel <stuartsippel@icloud.com>
2019-01-17 17:19:02 -0600

    Setup storyboard, book model, model controller

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,89 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Stuart on 1/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    
+    // Creating a new book instance and appending it to the books array
+    func createBook(with title: String, reasonToRead: String) {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        books.append(book)
+        
+        saveToPersistentStore()
+    }
+    
+    // Updating a specific book in our books array
+    func update(book: Book, title: String, reasonToRead: String, hasBeenRead: Bool) {
+        guard let index = books.index(of: book) else { return }
+        
+        books[index].title = title
+        books[index].reasonToRead = reasonToRead
+        books[index].hasBeenRead = hasBeenRead
+        
+        saveToPersistentStore()
+    }
+    
+    // Deleting a book from the books array and updating our persistent storage plist
+    func deleteBook(book: Book) {
+        guard let index = books.index(of: book) else { return }
+        books.remove(at: index)
+        
+        saveToPersistentStore()
+    }
+    
+    // Saving books in persistent storage
+    private func saveToPersistentStore() {
+        do {
+            let plistEncoder = PropertyListEncoder()
+            
+            let booksData = try plistEncoder.encode(books)
+            
+            guard let booksFileURL = booksFileURL else { return }
+            
+            try booksData.write(to: booksFileURL)
+        } catch {
+            NSLog("Error encoding books: \(error)")
+        }
+    }
+    
+    // Loading books for persistent storage
+    private func loadFromPersistentStore() {
+        do {
+            guard let booksFileURL = booksFileURL,
+                FileManager.default.fileExists(atPath: booksFileURL.path) else { return }
+            
+            let booksData = try Data(contentsOf: booksFileURL)
+            
+            let plistDecoder = PropertyListDecoder()
+            
+            self.books = try plistDecoder.decode([Book].self, from: booksData)
+        } catch {
+            NSLog("Error decoding books: \(error)")
+        }
+    }
+
+    // File directory for our books.plist file
+    private var booksFileURL: URL? {
+        guard let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        let fileName = "books.plist"
+        
+        return documentDirectory.appendingPathComponent(fileName)
+    }
+    
+    var readBooks: [Book] {
+        return books.filter { $0.hasBeenRead }
+    }
+    
+    var unreadBooks: [Book] {
+        return books.filter { !$0.hasBeenRead }
+    }
+    
+    var books: [Book] = []
+}
