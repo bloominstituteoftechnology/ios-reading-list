3aaa4a9d827bc042ac8b26cd9ad652719dfdacd9
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-17 13:48:47 -0800

    Set up table vc and custom cell class with UI.

---
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Nathanael Youngren on 1/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    
+    @IBAction func bookButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var bookLabel: UILabel!
+    @IBOutlet weak var bookButton: UIButton!
+    
+    
+}
