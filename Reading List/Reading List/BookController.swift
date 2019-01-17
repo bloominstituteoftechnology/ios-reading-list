32d06b6b0226bf8557477a8fce7c04e0dfc4d7e5
Vijay Das <vijaytdas@gmail.com>
2019-01-17 17:18:56 -0600

    add persistent store methods for save and load in BooksController

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -8,4 +8,60 @@
 
 import Foundation
 
+class BookController {
+    
+    private(set) var books: [Book] = []
+    
+    
+    
+
+    
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            return nil
+        }
+        
+        let finalLocation = documentsDirectory.appendingPathComponent("ReadingList.plist")
+        
+        return finalLocation
+    }
+    
+    func saveToPersistentStore() {
+        
+        guard let url = readingListURL else { return }
+        
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let booksData = try encoder.encode(books)
+            try booksData.write(to: url)
+        } catch {
+            print(error)
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        
+        guard let url = readingListURL, FileManager.default.fileExists(atPath: url.path) else { return }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            books = try decoder.decode([Book].self, from: data)
+            
+        } catch {
+            print(error)
+        }
+        
+  
+    }
+    
+
+}
+    
+    
+  
+    
+    
 
