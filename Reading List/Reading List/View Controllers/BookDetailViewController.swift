0d8e3f92b070a30215f3047c2167b3c00f69467c
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-17 15:33:52 -0800

    Add functionality to Book Detail VC.

--- Reading List/Reading List/View Controllers/BookDetailViewController.swift
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -11,10 +11,32 @@ import UIKit
 class BookDetailViewController: UIViewController {
 
     @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        guard let title = titleTextField.text else { return }
+        
+        if book == nil {
+            bookController?.createBook(title: title, reasonToRead: reasonTextView.text)
+        } else {
+            bookController?.updateTitleReasonToRead(for: book!, title: title, reasonToRead: reasonTextView.text)
+        }
+    }
     
+    func updateViews() {
+        guard let unwrappedBook = book else { return }
+        
+        titleTextField.text = unwrappedBook.title
+        reasonTextView.text = unwrappedBook.reasonToRead
+        
+        if unwrappedBook.title != "" {
+            navigationItem.title = unwrappedBook.title
+        } else {
+            navigationItem.title = "Add a new book"
+        }
     }
     
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var reasonTextView: UITextView!
+    
+    var bookController: BookController?
+    var book: Book?
     
 }
