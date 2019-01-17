b9d37f5cab6a9d50445568de13677db1317f3bdd
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-17 12:05:43 -0800

    Create Book data model.

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,21 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Nathanael Youngren on 1/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Codable {
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: Bool?
+    
+    init(title: String, reasonToRead: String, hasBeenRead: Bool? = false) {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
+}
