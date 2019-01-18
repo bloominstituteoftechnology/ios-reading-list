fe7cdfe25874d5d02bcf861c72d08308fd14c6e8
nateyoungren <nathanaelyoungren@gmail.com>
2019-01-17 19:51:50 -0800

    Add functions and properties to Detail VC.

--- Reading List again/Reading List again/BookDetailViewController.swift
+++ Reading List again/Reading List again/BookDetailViewController.swift
@@ -12,19 +12,42 @@ class BookDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        guard let title = titleTextField.text else { return }
+        
+        if book == nil {
+            bookController?.createBook(title: title, reason: reasonTextView.text)
+            navigationController?.popViewController(animated: true)
+        } else {
+            bookController?.updateTitleReason(for: book!, title: title, reason: reasonTextView.text)
+            navigationController?.popViewController(animated: true)
+        }
     }
-    */
+    
+    func updateViews() {
+        guard let unwrappedBook = book else { return }
+        
+        titleTextField.text = unwrappedBook.title
+        reasonTextView.text = unwrappedBook.reasonToRead
+        
+        if unwrappedBook.title != "" {
+            navigationItem.title = unwrappedBook.title
+        } else {
+            navigationItem.title = "Add a new book"
+        }
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonTextView: UITextView!
+
+    var bookController: BookController?
+    var book: Book? {
+        didSet {
+            updateViews()
+        }
+    }
 
 }
