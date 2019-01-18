9aec7d6fdfcc90a1d804c4ba31229bbf94dece3d
Angel <angelbuenrostro@outlook.com>
2019-01-18 01:56:51 -0500

    Add PhotoDetailViewController to storyboard

---
+++ Reading List/Reading List/Views/BookDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Angel Buenrostro on 1/18/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Reading List/Reading List/Views/BookTableViewCell.swift
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class BookTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var bookLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    @IBAction func checkButtonTapped(_ sender: Any) {
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
