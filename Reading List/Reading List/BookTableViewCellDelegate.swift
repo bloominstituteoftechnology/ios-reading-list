eb5c8c5f1fe275e245b409ddbf4f23d68b173581
Paul Yi <yipaulc@gmail.com>
2019-01-24 16:47:42 -0800

    Use delegate pattern to communicate with table view controller

---
+++ Reading List/Reading List/BookTableViewCellDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  BookTableViewCellDelegate.swift
+//  Reading List
+//
+//  Created by Lambda_School_Loaner_59 on 1/24/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol BookTableViewCellDelegate {
+    func toggleHasBeenRead(for cell: BookTableViewCell)
+}
