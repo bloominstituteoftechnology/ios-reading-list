129e0561818bbaec8dbe7df18a45a5cf93a0ee36
Paul Yi <yipaulc@gmail.com>
2019-01-24 18:09:32 -0800

    Set table view up to use two sections

--- Reading List/Reading List/Model/Model Controller/BookController.swift
+++ Reading List/Reading List/Model/Model Controller/BookController.swift
@@ -43,7 +43,7 @@ class BookController {
         saveToPersistentStore()
     }
     
-    func updateHasBeenread(for book: Book) {
+    func updateHasBeenRead(for book: Book) {
         guard let index = books.index(of: book) else { return }
         
         var book = book
