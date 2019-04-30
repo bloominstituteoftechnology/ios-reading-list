12ac2353be1707483476054939550acec0880cb7
victor <ruizdvictor@gmail.com>
2019-04-30 16:06:07 -0400

    updated table view cells to incorporate delegate pattern

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Victor  on 4/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    @IBOutlet weak var bookTextField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func saveButtonPressed(_ sender: Any) {
+        //handle when save button is pressed
+    }
+}
--- Reading List/Reading List/BookTableViewCell.swift
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -13,9 +13,24 @@ class BookTableViewCell: UITableViewCell {
     @IBOutlet weak var bookLabel: UILabel!
     @IBOutlet weak var isReadButton: UIButton!
     
+    weak var delegate: BookTableViewCellDelegate?
+    var book: Book?
+    
+    func updateViews() {
+        guard let book = book else {return}
+        bookLabel.text = book.title
+        if book.hasBeenRead {
+            isReadButton.setImage(UIImage(named: "checked"), for: .normal)
+        } else {
+            isReadButton.setImage(UIImage(named: "Unchecked"), for: .normal)
+        }
+    }
+    
     
     @IBAction func isReadButtonPressed(_ sender: Any) {
         //handle when button is pressed
-        
+        delegate?.toggleHasBeenRead(for: self)
     }
+    
+    
 }
