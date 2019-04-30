128e7d5fafc0bdf36c11ec2bb244e4c7eb5063b3
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-30 13:32:36 -0700

    created Computed property readingListURL: URL?, return  url of file path

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -12,5 +12,25 @@ class BookController {
 	
 	
 	
+	
+	
+	
+	
+	
+	private var readingListURL: URL? {
+		//Get the user's document directory using the FileManager class.
+		let fileManager = FileManager.default
+		guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+		
+		//Create a filename string for the plist, such as "ReadingList.plist"
+		let fileName = "ReadingList.plist"
+		
+		//Return a url that appends the filename string to the document directory.
+		let document = documents.appendingPathComponent(fileName)
+		
+		print(document)
+		return document
+	}
+	
 	private(set) var books: [Book] = []
 }
