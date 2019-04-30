60c3a6fd5ee32b45f909dba46dca025f9dbec823
Dan Morse <morsedan@outlook.com>
2019-04-29 20:10:05 -0700

    Finished up storyboard. Still need to add outlets and actions.

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by morse on 4/29/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
