2aaa757c3f2a2f1e5e511c59e12e0350171e6508
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-30 13:21:27 -0700

    BookController created with a private [Book]

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,16 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Hector Steven on 4/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+	
+	
+	
+	private(set) var books: [Book] = []
+}
