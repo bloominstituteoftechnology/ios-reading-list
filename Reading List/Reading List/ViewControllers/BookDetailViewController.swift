f75bdb86348d5eddb5901f352d0a821610c8db86
hectorsvill <hectorstevenvillasano@gmail.com>
2019-04-30 19:15:40 -0700

    StoryBoard scenes created and IB's connected

---
+++ Reading List/Reading List/ViewControllers/BookDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Hector Steven on 4/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+	
+	@IBAction func saveBarButtonPressed(_ sender: UIBarButtonItem) {
+		print("save")
+	
+	}
+	
+	@IBOutlet var bookTitleTextField: UITextField!
+	@IBOutlet var reasonToReadTextView: UITextView!
+	
+}
