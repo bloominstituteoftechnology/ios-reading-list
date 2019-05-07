a279f5ac9b2cd332ac1fe7ec611c42c52d2a2b9c
Austin Martin <armartin2300@gmail.com>
2019-05-07 12:15:11 -0700

    Finished adding files and connecting all UI elements.

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Lambda_School_Loaner_64 on 5/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        
+        
+        
+    }
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var addBookTextField: UITextField!
+    @IBOutlet weak var detailsTextView: UITextView!
+
+}
