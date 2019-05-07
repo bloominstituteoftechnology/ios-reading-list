a5c75731a0f60060235feb2bce13cb9465c01ae0
Thomas Cacciatore <tcacciatore597@gmail.com>
2019-05-07 15:53:09 -0400

    Set up most of the code in the Model Book and BookController files.  Did not complete functions yet for deleting a book or updating the title and reason to read.

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,23 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Thomas Cacciatore on 5/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Codable {
+    
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: Bool
+    
+    init(title: String, reasonToRead: String, hasBeenRead: Bool = false) {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
+    
+}
