706d88a535e3fa760e57748e07d8b01981f02f32
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-07 13:18:52 -0600

    Created BookController.swift
    Created a computed property of readingListURL in BookController.swift to initiate persistence.

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,20 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Mitchell Budge on 5/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] = []
+    
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        print("Documents: \(documents.path)")
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+}
