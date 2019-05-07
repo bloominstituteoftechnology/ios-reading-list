c8ca32b2b7aef87c0159ff9998fe9937da1de773
Taylor Lyles <hawks93187@gmail.com>
2019-05-07 14:10:58 -0700

    finished part 1

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -21,7 +21,7 @@ class BookController: Codable {
 		return documents.appendingPathComponent("ReadingList.plist")
 	}
 	
-	func saveToPersistenStore() {
+	func saveToPersistentStore() {
 		guard let url = readingListURL else { return }
 		
 		let encoder = PropertyListEncoder()
@@ -35,7 +35,7 @@ class BookController: Codable {
 		
 	}
 	
-	func loadFromPersistenStore() {
+	func loadFromPersistentStore() {
 		do {
 			guard let url = readingListURL else { return }
 			let data = try Data(contentsOf: url)
@@ -45,6 +45,45 @@ class BookController: Codable {
 			print("Error loading data from disk: \(error)")
 		}
 	}
+	
+	func create(title: String, reasonToRead: String, hasBeenRead: Bool) {
+		let book = Book(title: title, reasonToRead: reasonToRead, hasBeenRead: hasBeenRead)
+		books.append(book)
+		saveToPersistentStore()
+	}
+	
+	
+	func delete(book: Book) {
+		guard let index = books.firstIndex(of: book) else { return }
+		books.remove(at: index)
+		saveToPersistentStore()
+	}
+	
+	func updateHasBeenRead(for book: Book) {
+		guard let index = books.firstIndex(of: book) else { return }
+		books[index].hasBeenRead = !books[index].hasBeenRead
+		saveToPersistentStore()
+	}
+	
+	func updatedTitleAndOrReasonToRead(book: Book, title: String, reasonToRead: String) {
+		guard let index = books.firstIndex(of: book) else { return }
+		if (books[index].title != title) || (books[index].reasonToRead != reasonToRead) {
+			books[index].title = title
+			books[index].reasonToRead = reasonToRead
+		}
+		
+	}
+	
+	var readBooks: [Book] {
+		let beenRead = books.filter { $0.hasBeenRead == true }
+		return beenRead
+	}
+	
+	var unReadBooks: [Book] {
+		let notBeenRead = books.filter { $0.hasBeenRead == false }
+		return notBeenRead
+	}
+	
 	
 	
 }
