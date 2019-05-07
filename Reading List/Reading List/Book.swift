66d5502c7785bad0eec6b3ca97c3e7d5df313c48
Taylor Lyles <hawks93187@gmail.com>
2019-05-07 13:15:27 -0700

    added Book.swift file

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,24 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Taylor Lyles on 5/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+struct Book: Equatable, Codable {
+	
+	var title: String
+	var reasonToRead: String
+	var hasBeenRead: Bool
+	
+	init(title: String, reasonToRead: String, hasBeenRead: Bool = false) {
+		self.title = title
+		self.reasonToRead = reasonToRead
+		self.hasBeenRead = hasBeenRead
+	}
+}
