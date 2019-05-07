b134efb4705cb42e62f99e412277c96a2c20576a
Taylor Lyles <hawks93187@gmail.com>
2019-05-07 13:39:07 -0700

    Part 1, number 4

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -21,5 +21,19 @@ class BookController: Codable {
 		return documents.appendingPathComponent("ReadingList.plist")
 	}
 	
+	func saveToPersistenStore() {
+		guard let url = readingListURL else { return }
+		
+		let encoder = PropertyListEncoder()
+		
+		do {
+			let booksData = try encoder.encode(books)
+			try booksData.write(to: url)
+		} catch {
+			print("Error saving books: \(error)")
+		}
+		
+	}
+	
 	
 }
