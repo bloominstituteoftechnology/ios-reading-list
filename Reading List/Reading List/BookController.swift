1472bb3269950c7cb0906becbc06edfb6fe19c17
Mitchell Budge <mitchellbudge@gmail.com>
2019-05-07 13:33:50 -0600

    Added persistence methods
    saveToPersistentStore() and loadFromPersistentStore() methods were created within the BookController.swift class

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -16,5 +16,29 @@ class BookController {
         guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
         print("Documents: \(documents.path)")
         return documents.appendingPathComponent("ReadingList.plist")
+    } // end of computed property
+    
+    func saveToPersistentStore() {
+        guard let url = readingListURL else { return }
+        do {
+            let encoder = PropertyListEncoder()
+            let booksData = try encoder.encode(books)
+            try booksData.write(to: url)
+        }   catch {
+            print("Error saving books: \(error)")
+        }
+    } // end of save data function
+    
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        guard let url = readingListURL,
+            fileManager.fileExists(atPath: url.path) else { return }
+        do {
+            let booksData = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            books = try decoder.decode([Book].self, from: booksData)
+        } catch {
+            print("Error loading data from disk: \(error)")
+        }
     }
 }
