255909272ababa18eb15540d5f81c2cebebaa50d
Jordan Davis <jordan.m.davis92@gmail.com>
2019-05-08 10:17:39 -0400

    persistence methods
    created persistence methods to save and load data from my application.

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -7,3 +7,64 @@
 //
 
 import Foundation
+
+class bookController {
+    
+    init() {
+        loadFromPersistentStore()
+    }
+    
+    func addBook(titled title: String, withReason reasonToRead: String, with hasBeenRead: Bool) {
+        let book = Book(title: title, reasonToRead: title)
+        
+        books.append(book)
+        
+        saveToPersistentStore()
+    }
+    
+    //save data
+    
+    func saveToPersistentStore() {
+        
+        guard let url = readingListURL else { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(books)
+            try data.write(to: url)
+        } catch {
+            print("Error saving books: \(error)")
+        }
+    }
+    
+    
+    //load data
+    
+    func loadFromPersistentStore() {
+        
+        let fileManager = FileManager.default
+        guard let url = readingListURL, fileManager.fileExists(atPath: url.path) else { return }
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            books = try decoder.decode([Book].self, from: data)
+        } catch {
+            print("Error loading data from disk: \(error)")
+        }
+    }
+    
+    
+    
+    
+    //MARK: -Properties
+    
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        print("Documents: \(documents.path)")
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+    
+    private(set) var books: [Book] = []
+}
