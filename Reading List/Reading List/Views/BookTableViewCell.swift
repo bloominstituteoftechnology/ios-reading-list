f0885d1b8724b9b518da53a3c95ab6a9864f44c9
shillwil <alex.shillingford@icloud.com>
2019-06-11 17:17:34 -0600

    Part 3 steps 1-3 completed

--- Reading List/Reading List/Views/BookTableViewCell.swift
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -12,11 +12,25 @@ class BookTableViewCell: UITableViewCell {
     
     @IBOutlet weak var labelOutlet: UILabel!
     @IBOutlet weak var checkedButtonOutlet: UIButton!
+    var book: Book?
+    let checkedImage = UIImage(named: "checked")
+    let uncheckedImage = UIImage(named: "unchecked")
     
     
     @IBAction func checkedButtonPressed(_ sender: UIButton) {
         
     }
+    
+    func updateViews() {
+        labelOutlet.text = book?.title
+        if book?.hasBeenRead == true {
+            checkedButtonOutlet.setImage(checkedImage, for: .normal)
+        } else {
+            checkedButtonOutlet.setImage(uncheckedImage, for: .normal)
+        }
+    }
+    
+    
     
     
 }
