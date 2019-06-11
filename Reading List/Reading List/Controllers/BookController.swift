022a23beff343cc9aca210fee992245896b7c505
shillwil <alex.shillingford@icloud.com>
2019-06-11 13:49:19 -0600

    BookController created and steps 1-3 of BookController completed

---
+++ Reading List/Reading List/Controllers/BookController.swift
@@ -0,0 +1,27 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Alex Shillingford on 6/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] = []
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        
+        // Creates "/Users/shillwil/Documents" - documents directory path
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+            else { return nil }
+        
+        // Creates "/Users/shillwil/Documents/books.plist" - full file path
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+    
+    func saveToPersistentStore() {
+        
+    }
+}
