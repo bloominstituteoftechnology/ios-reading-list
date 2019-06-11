29746e094a7c15d9774c5071ab06cb1bc1a277a3
shillwil <alex.shillingford@icloud.com>
2019-06-11 16:36:06 -0600

    Part 2 step 6 completed

--- Reading List/Reading List/Views/BookTableViewCell.swift
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -9,16 +9,14 @@
 import UIKit
 
 class BookTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    @IBOutlet weak var labelOutlet: UILabel!
+    @IBOutlet weak var checkedButtonOutlet: UIButton!
+    
+    
+    @IBAction func checkedButtonPressed(_ sender: UIButton) {
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    
 }
