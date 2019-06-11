b068094e36f7705f8588b69ef95803d5b77a5c4a
shillwil <alex.shillingford@icloud.com>
2019-06-11 16:33:29 -0600

    Part 2 Step 4 and 5 completed

---
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Alex Shillingford on 6/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
