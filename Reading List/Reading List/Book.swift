58519b52d8765377faeda7e65a0e20a662caf999
Kennon Ward <kennon.mward@gmail.com>
2019-06-12 09:53:31 -0400

    part 1 completed..

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,21 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Kennon Keys Ward on 6/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Codable {
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: Bool
+    
+    init(title: String, reasonToRead: String, hasBeenRead: Bool = false) {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
+}
