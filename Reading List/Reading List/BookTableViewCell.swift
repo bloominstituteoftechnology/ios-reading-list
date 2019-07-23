abc3f7274b8a77da5cca560b9bea883c6cbf42bf
bradleyyin <bradleyyin@gmail.com>
2019-07-23 15:26:22 -0700

    connects view with vc

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Bradley Yin on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    
+    
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+    }
+    
+
+}
