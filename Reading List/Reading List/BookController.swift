f88173567c8b715d4bbc3bbb352a4e2236c4282f
Taylor Lyles <hawks93187@gmail.com>
2019-07-23 15:23:17 -0700

    Added reading url, saving & loading to persistent store functions

--- Reading List/Reading List/BookController.swift
+++ Reading List/Reading List/BookController.swift
@@ -8,8 +8,43 @@
 
 import Foundation
 
-class BookController {
+class BookController: Codable {
 	
 	private(set) var books = [Book]()
+	
+	// MARK: - Persistence
+	
+	func saveToPersistentStore() {
+		guard let url = readingListURL else { return }
+		
+		do {
+			let encoder = PropertyListEncoder()
+			let booksData = try encoder.encode(books)
+			try booksData.write(to: url)
+		} catch {
+			print("Error saving books data: \(error)")		}
+	}
+	
+	func loadToPersistentStore() {
+		let fm = FileManager.default
+		guard let url = readingListURL,
+			fm.fileExists(atPath: url.path) else { return }
+		
+		do {
+			let data = try Data(contentsOf: url)
+			let decoder = PropertyListDecoder()
+			books = try decoder.decode([Book].self, from: data)
+		} catch {
+			print("Error loading books data: \(error)")
+		}
+	}
+	
+	// MARK: - Private
+	
+	private var readingListURL: URL? {
+		let fm = FileManager.default
+		guard let dir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+		return dir.appendingPathComponent("books.plist")
+	}
 	
 }
