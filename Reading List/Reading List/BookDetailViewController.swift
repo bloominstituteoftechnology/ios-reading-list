4d6daa9eca97b9c8ba6078d8806e231a55013a3b
Taylor Lyles <hawks93187@gmail.com>
2019-07-23 16:27:22 -0700

    update views method in TVCell

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Taylor Lyles on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+	
+	@IBOutlet weak var textField: UITextField!
+	@IBOutlet weak var textView: UITextView!
+	
+	@IBAction func saveButton(_ sender: UIButton) {
+	}
+	
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+	
+
+}
--- Reading List/Reading List/BookTableViewCell.swift
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class BookTableViewCell: UITableViewCell {
-
+	
+	var book: Book?
 	
 	@IBOutlet weak var title: UILabel!
 	@IBOutlet weak var readButton: UIButton!
@@ -17,5 +18,18 @@ class BookTableViewCell: UITableViewCell {
 	@IBAction func checkReadButton(_ sender: UIButton) {
 	}
 	
+	private func updateViews() {
+		guard let book = book else { return }
+		title.text = book.title
+		
+		var image: UIImage!
+		
+		if book.hasBeenRead {
+			image = UIImage(named: "checked")
+		} else {
+			image = UIImage(named: "unchecked")
+		}
+		readButton.setImage(image, for: .normal)
+	}
 
 }
