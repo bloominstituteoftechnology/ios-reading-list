04be63b40383bf1622886d488669783524bf2d24
Taylor Lyles <hawks93187@gmail.com>
2019-07-23 15:07:47 -0700

    Added Book Controller

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,15 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Taylor Lyles on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+	
+	private(set) var books = [Book]()
+	
+}
