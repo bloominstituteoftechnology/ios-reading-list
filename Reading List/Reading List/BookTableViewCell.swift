bd314180f281b55b87e233e1b60b8fc3bc8e29bb
shillwil <alex.shillingford@icloud.com>
2019-07-23 16:20:12 -0600

    Part 2 completed

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Alex Shillingford on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookLabel: UILabel!
+    @IBOutlet weak var checkButton: UIButton!
+    
+    
+    @IBAction func checkButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+}
