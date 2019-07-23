c460aa1ad98aa4ef8a8533546ee15b9fb2b8da64
Taylor Lyles <hawks93187@gmail.com>
2019-07-23 16:11:39 -0700

    set up outlets to cell VC

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Taylor Lyles on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+	
+	@IBOutlet weak var title: UILabel!
+	@IBOutlet weak var readButton: UIButton!
+	
+	@IBAction func checkReadButton(_ sender: UIButton) {
+	}
+	
+
+}
---
+++ Reading List/Reading List/ReadingListTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReadingListTableViewController.swift
+//  Reading List
+//
+//  Created by Taylor Lyles on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ReadingListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
