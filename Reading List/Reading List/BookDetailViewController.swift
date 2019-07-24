759577730e4ce0bd0f556444616b7f764a73d6ea
JerryHaaser <jerry.haaser@hotmail.com>
2019-07-23 22:27:37 -0500

    Added text field and text and save button  views to detail view controller, added appropriate outlets and action

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,38 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by admin on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var reasonForReadingTextView: UITextView!
+    
+    
+    @IBAction func saveButton(_ sender: UIBarButtonItem) {
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
