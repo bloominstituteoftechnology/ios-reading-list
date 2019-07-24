1c9e56694904dcfafb374c1b457378fd273513eb
Blake Andrew Price <blakeandrewprice@gmail.com>
2019-07-23 18:08:58 -0700

    Part 2 complete, build succeeds

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Reading List
+//
+//  Created by Spencer Curtis on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+}
+
---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Blake Andrew Price on 7/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+}
+
+
