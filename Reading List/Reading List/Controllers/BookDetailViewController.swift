bcc248d86ac832ee113f8b9d5edb5a09ffb21250
John Kouris <jdkouris@gmail.com>
2019-08-10 09:43:27 -0700

    Finished designing the book detail screen, added the BookDetailViewController, and wired up outlets and save action

---
+++ Reading List/Reading List/Controllers/BookDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by John Kouris on 8/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var reasonToReadTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+
+}
