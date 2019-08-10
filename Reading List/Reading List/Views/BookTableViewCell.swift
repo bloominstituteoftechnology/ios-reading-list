5a35678544e2ea99e25b25f30a77a2cc17737e8d
John Kouris <jdkouris@gmail.com>
2019-08-10 09:36:02 -0700

    Added the outlets for the custom cell and removed the starter code from the BookTableViewCell class

--- Reading List/Reading List/Views/BookTableViewCell.swift
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -10,15 +10,12 @@ import UIKit
 
 class BookTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var hasBeenReadButton: UIButton!
+    
+    
+    @IBAction func hasBeenReadButtonTapped(_ sender: Any) {
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
 }
