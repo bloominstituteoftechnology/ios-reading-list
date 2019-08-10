700006bd48ac929a70e34a71c8056658b432f702
John Kouris <jdkouris@gmail.com>
2019-08-10 09:50:41 -0700

    Added updateViews() method to update the titleLabel text and determine if the unchecked or checked button should be shown

--- Reading List/Reading List/Views/BookTableViewCell.swift
+++ Reading List/Reading List/Views/BookTableViewCell.swift
@@ -13,9 +13,22 @@ class BookTableViewCell: UITableViewCell {
     @IBOutlet weak var bookTitleLabel: UILabel!
     @IBOutlet weak var hasBeenReadButton: UIButton!
     
+    var book: Book?
     
     @IBAction func hasBeenReadButtonTapped(_ sender: Any) {
         
+    }
+    
+    func updateViews() {
+        guard let book = book else { return }
+        
+        bookTitleLabel.text = book.title
+        
+        if book.hasBeenRead {
+            hasBeenReadButton.imageView?.image = UIImage(named: "checked")
+        } else {
+            hasBeenReadButton.imageView?.image = UIImage(named: "unchecked")
+        }
     }
     
 }
