fd066aa11d846867c8897a76e05a77a1f6b59093
dillonp23 <dillonp.lambda@gmail.com>
2019-08-09 19:16:43 -0700

    Add Book.swift for 'Book' struct with properties and initializer for default value

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,21 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Dillon P on 8/9/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Codable, Equatable {
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: Bool
+    
+    init(title: String, reasonToRead: String, hasBeenRead: Bool = false) {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
+}
