883b670feb5ddc0894d0fb715fb75964d1a7a7f1
JosephRogers94 <Joerogersss2016@gmail.com>
2019-08-11 01:17:18 -0500

    Finish Storyboard

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Joseph Rogers on 8/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    @IBOutlet weak var detailTextView: UITextView!
+    @IBOutlet weak var detailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Joseph Rogers on 8/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookNameLabel: UILabel!
+    @IBOutlet weak var checkListButton: UIButton!
+    //    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        // Initialization code
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+
+    
+    @IBAction func checkListPressed(_ sender: UIButton) {
+        
+    }
+    
+    
+}
