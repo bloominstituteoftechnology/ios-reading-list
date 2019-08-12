9e6adbd565757dd1fe77bfc1266a611f5cd6f139
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-11 21:20:32 -0400

    Completed steps 1, 2, and 3 of "BookController.swift" file

---
+++ Reading List/Reading List/Controllers/BookController.swift
@@ -0,0 +1,21 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Jessie Ann Griffin on 8/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    // initialize an empty array of books
+    private(set) var books: [Book] = []
+    // create a computed property where the list will be saved on the device
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+    
+}
