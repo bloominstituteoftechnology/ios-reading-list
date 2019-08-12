4eb753fcf54b0e2dc58c9d75c167be1f61e97b42
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-11 23:03:56 -0400

    Part Two complete

---
+++ Reading List/Reading List/Custom Views/BookTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Jessie Ann Griffin on 8/11/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var bookReadCheckBox: UIButton!
+    
+    
+    @IBAction func hasBeenReadPressed(_ sender: UIButton) {
+    }
+    
+}
--- Reading List/Reading List/View Controllers/BookDetailViewController.swift
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class BookDetailViewController: UIViewController {
 
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func saveButtonPressed(_ sender: Any) {
     }
-
-
 }
 
