a718177e95a92881da90a82471c0c1dc1a005cd0
Eoin Lavery <dev@tenantc.co.uk>
2019-08-12 02:33:31 +0100

    Added Stretch Goal: Change cell colour depending on hasBeenRead status

--- Reading List/Reading List/Custom Views/BookTableViewCell.swift
+++ Reading List/Reading List/Custom Views/BookTableViewCell.swift
@@ -21,14 +21,18 @@ class BookTableViewCell: UITableViewCell {
     }
     
     func updateViews() {
+        let greenCellColor = UIColor(red: 0.749, green: 0.8784, blue: 0.102, alpha: 1.0)
+        let redCellColor = UIColor(red: 1, green: 0.5686, blue: 0.5686, alpha: 1.0)
         guard let book = book else { return }
         titleLabel.text = book.title
         if book.hasBeenRead == true {
             guard let image = UIImage(named: "checked") else { return }
             hasBeenReadButton.setImage(image, for: .normal)
+            self.backgroundColor = greenCellColor
         } else {
             guard let image = UIImage(named: "unchecked") else { return }
             hasBeenReadButton.setImage(image, for: .normal)
+            self.backgroundColor = redCellColor
         }
     }
     
