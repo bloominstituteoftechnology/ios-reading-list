df2e88a0899afb3536440e213098718cd7652fb0
Jerry Haaser <jerry.haaser@hotmail.com>
2019-08-20 14:59:51 -0500

    Created Book model and BookController

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,68 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by admin on 8/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+
+    var books: [Book] = []
+    
+    private var readingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+            else { return nil }
+        
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+    
+    var readBooks: [Book] {
+        
+    }
+    
+    var unreadBooks: [Book] {
+        
+    }
+    
+    
+    
+    func createBook(titled title: String, withReasonRead reasonToRead: String, withHasBeenRead hasBeenRead: Bool ) -> Book {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        books.append(book)
+        saveToPersistentStore()
+        return book
+    }
+    
+    
+    func saveToPersistentStore() {
+        guard let url = readingListURL else { return }
+        
+        do {
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(books)
+            try data.write(to: url)
+        } catch {
+            print("Error saving books data: \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        guard let url = readingListURL, fileManager.fileExists(atPath: url.path) else { return }
+        
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            books = try decoder.decode([Book].self, from: data)
+        } catch {
+            print("Error loading books data: \(error)")
+        }
+    }
+    
+    
+    
+}
