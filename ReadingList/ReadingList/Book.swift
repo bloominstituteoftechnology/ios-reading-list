68f5d0f4374a3cfe4e1dccf9d1112022d0be4183
Joshua Franklin <jfranklin1607@gmail.com>
2019-08-20 16:43:25 -0400

    Added struct file

---
+++ ReadingList/ReadingList/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  ReadingList
+//
+//  Created by Joshua Franklin on 8/20/19.
+//  Copyright © 2019 AMSU. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: Bool
+    
+}
