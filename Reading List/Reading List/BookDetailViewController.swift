2434cb91a6ed81c76fb903a91b154e3f588c276c
Jordan Christensen <jordan.c4922@gmail.com>
2019-08-21 12:33:25 +0800

    Not finished

--- Reading List/Reading List/BookDetailViewController.swift
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -13,6 +13,23 @@ class BookDetailViewController: UIViewController {
     @IBOutlet weak var nameLabel: UITextField!
     @IBOutlet weak var reasonTextField: UITextView!
     
+    var bookController = BookController()
+    var book: Book? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    func updateViews() {
+        guard let book = book else {
+            self.title = "Add New Book"
+            return
+        }
+        nameLabel.text = book.title
+        reasonTextField.text = book.reasonToRead
+        self.title = book.title
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,8 +41,15 @@ class BookDetailViewController: UIViewController {
     }
 
     @IBAction func saveBook(_ sender: UIButton) {
-        
+        if let name = nameLabel.text, let reason = reasonTextField.text {
+            if book == nil {
+                bookController.createBook(title: name, reasonToRead: reason)
+            } else {
+                guard let book = book else { return }
+                bookController.setTitleReasonToRead(bookToUpdate: book, name, reason)
+            }
+            navigationController?.popViewController(animated: true)
+        }
     }
-    
 }
 
--- Reading List/Reading List/ReadingListTableViewController.swift
+++ Reading List/Reading List/ReadingListTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ReadingListTableViewController: UITableViewController {
+    
+    let bookController = BookController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,69 +26,60 @@ class ReadingListTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch (section) {
+        case 0:
+            return bookController.readBooks.count
+        case 1:
+            return bookController.unreadBooks.count
+        default:
+            return 0
+        }
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "BookCell", for: indexPath) as? BookTableViewCell else { return UITableViewCell() }
+        var book: Book?
+        switch (indexPath.section) {
+        case 0:
+            book = bookController.readBooks[indexPath.row]
+        case 1:
+            book = bookController.unreadBooks[indexPath.row]
+        default:
+            return UITableViewCell()
+        }
+        cell.delegate = self
+        cell.book = book
         return cell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
+        switch (section) {
+        case 0:
+            return "\(bookController.readBooks.count) books read"
+        case 1:
+            return "\(bookController.unreadBooks.count) books to read"
+        default:
+            return ""
+        }
     }
+}
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+extension ReadingListTableViewController: BookTableViewCellDelegate {
+    func toggleHasBeenRead(for cell: BookTableViewCell) {
+        if let indexPath = tableView.indexPath(for: cell) {
+            switch (indexPath.section) {
+            case 0:
+                bookController.updateHasBeenRead(for: bookController.readBooks[indexPath.row])
+            case 1:
+                bookController.updateHasBeenRead(for: bookController.unreadBooks[indexPath.row])
+            default:
+                break
+            }
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
