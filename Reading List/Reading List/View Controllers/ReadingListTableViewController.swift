8d3f4f8207362878f90957bffdee9cd48a574c45
Jon Bash <jonbashmusic@gmail.com>
2019-10-15 14:22:34 -0700

    reorganize structure

--- Reading List/Reading List/View Controllers/ReadingListTableViewController.swift
+++ Reading List/Reading List/View Controllers/ReadingListTableViewController.swift
@@ -23,13 +23,13 @@ class ReadingListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch section {
+        case 0: return 
+        }
     }
 
     /*
