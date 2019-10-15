ea770fc303fad5ba769779f2cd1a60ac6dfdcf45
Dan M <djmthedev@gmail.com>
2019-10-15 12:15:53 -0700

    added persistence methods to BookController.

--- Reading List/Reading List/Model Controllers/BookController.swift
+++ Reading List/Reading List/Model Controllers/BookController.swift
@@ -13,5 +13,35 @@ class BookController {
     
     
     
+    private var persistentFileURL: URL? {
+        let fm = FileManager.default
+        guard let dir = fm.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return dir.appendingPathComponent("books.plist")
+    }
     
+    private func saveToPersistentStore() {
+        guard let url = persistentFileURL else { return }
+        
+        do{
+            let encoder = PropertyListEncoder()
+            let data = try encoder.encode(books)
+            try data.write(to: url)
+        } catch {
+            print("Error saving books data: \(error)")
+        }
+    }
+    
+    private func loadFromPersistentStore() {
+        let fm = FileManager.default
+        guard let url = persistentFileURL,
+            fm.fileExists(atPath: url.path) else { return }
+        
+        do{
+            let data = try Data(contentsOf: url)
+            let decoder = PropertyListDecoder()
+            books = try decoder.decode([Book].self, from: data)
+        } catch {
+            print("Error loading books data: \(error)")
+        }
+    }
 }
