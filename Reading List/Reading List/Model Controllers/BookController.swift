b358f054862c5fd6baf3dc84638f6212b363003e
Dan M <djmthedev@gmail.com>
2019-10-15 12:15:26 -0700

    added BookController

---
+++ Reading List/Reading List/Model Controllers/BookController.swift
@@ -0,0 +1,17 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by morse on 10/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] = []
+    
+    
+    
+    
+}
