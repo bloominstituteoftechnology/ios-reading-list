383d0fc5156664a1e44ebb605965b88b91eeba33
Thomas Sabino-Benowitz <sabinowitz@gmail.com>
2019-10-16 11:57:16 -0400

    added tableviewcelldelegate, created func updateHasBeenRead

---
+++ Reading List/Reading List/BookTableViewCellDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  BookTableViewCellDelegate.swift
+//  Reading List
+//
+//  Created by Thomas Sabino-Benowitz on 10/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
