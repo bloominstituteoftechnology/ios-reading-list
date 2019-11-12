4d73c9cbb5b055081399e7fbabb801aa1a6669a0
Chad Rutherford <chad_rutherford@outlook.com>
2019-11-12 15:49:35 -0500

    Added ' Book' model

---
+++ Reading List/Reading List/Model/Model Classes/Book.swift
@@ -0,0 +1,21 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Chad Rutherford on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Codable {
+    var name: String
+    var reasonToRead: String
+    var hasBeenRead: Bool
+    
+    init(name: String, reasonToRead: String, hasBeenRead: Bool = false) {
+        self.name = name
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
+}
