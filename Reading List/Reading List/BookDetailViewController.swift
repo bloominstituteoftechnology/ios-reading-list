9de6aeb1dbe5a4abc3884a88bb4d1b74a0e0d66c
donellabarcelo <donellabarcelo95@gmail.com>
2019-11-12 15:11:50 -0800

    Created IB outlets and actions to detail view controller file

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Donella Barcelo on 11/12/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonToReadTextView: UITextView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+}
