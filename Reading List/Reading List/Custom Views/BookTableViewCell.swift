46cc86d9640889d841adf7609717d3286fd9e19c
Jeremy Taylor <jtaylor@jctaylor.org>
2019-12-06 12:49:47 -0600

    Completed implementation of BookTableViewCell

--- Reading List/Reading List/Custom Views/BookTableViewCell.swift
+++ Reading List/Reading List/Custom Views/BookTableViewCell.swift
@@ -13,7 +13,27 @@ class BookTableViewCell: UITableViewCell {
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var hasBeenReadButton: UIButton!
     
+     var delegate: BookTableViewCellDelegate?
+    
+    var book: Book? {
+        didSet {
+            updateViews()
+        }
+    }
+    
     @IBAction func toggleHasBeenRead(_ sender: UIButton) {
+        delegate?.toggleHasBeenRead(for: self)
+    }
+    
+    private func updateViews() {
+        guard let book = book else { return }
         
+        titleLabel.text = book.title
+        
+        if book.hasBeenRead {
+            hasBeenReadButton.setImage(UIImage(named: "checked"), for: .normal)
+        } else {
+           hasBeenReadButton.setImage(UIImage(named: "unchecked"), for: .normal)
+        }
     }
 }
