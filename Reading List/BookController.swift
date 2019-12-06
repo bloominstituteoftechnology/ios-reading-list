8326f3371b26be2e8e5f79ce2e32c690e6ea0205
Osha Washington <washingtonosha209@yahoo.com>
2019-12-06 01:59:27 -0800

    Save, Load, Create, Delete, Updates, and new title created

---
+++ Reading List/BookController.swift
@@ -0,0 +1,81 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Osha Washington on 12/6/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    
+    init() {
+        loadFromPersistentStore()
+    }
+   
+    private (set) var books: [Book] = []
+    
+    private var persistentReadingListURL: URL? {
+        let fileManager = FileManager.default
+        guard let documents = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+    
+    // MARK: - SAVE
+    func saveToPersistentStore() {
+        guard let url = persistentReadingListURL else { return }
+        
+        do {
+            let encoder =  PropertyListEncoder()
+            let booksData = try encoder.encode(books)
+            try booksData.write(to: url)
+        } catch {
+            print("Error saving books data: \(error)")
+        }
+}
+    
+    // MARK: - LOAD
+    func loadFromPersistentStore() {
+        let fileManager = FileManager.default
+        guard let url = persistentReadingListURL, fileManager.fileExists(atPath: url.path) else { return }
+        
+        do {
+            let bookData = try Data(contentsOf: url)
+            let decodeBooks = PropertyListDecoder()
+            books = try decodeBooks.decode([Book].self, from: bookData)
+        } catch {
+            print("Error loading books data: \(error)")
+        }
+    }
+    
+    //MARK: - CREATE
+    func createBook(title: String, reasonToRead: String) {
+        let createdBook = Book(title: title, reasonToRead: reasonToRead)
+        books.append(createdBook)
+        saveToPersistentStore()
+    }
+    
+    // MARK: - DELETE
+    func deleteBook(_ book: Book) {
+        guard let index = books.index(of: book) else { return }
+        books.remove(at: index)
+        saveToPersistentStore()
+    }
+    
+    // MARK: - UPDATE hasBeenRead
+    func hasBeenRead(for book: Book) {
+        guard let index = books.index(of: book) else { return }
+        books[index].hasBeenRead.toggle()
+        saveToPersistentStore()
+    }
+    
+    //MARK: - UPDATE updateBookTitle
+    func updateBookTitle(for book: Book, newTitle: String, newReasonToRead: String) {
+        guard let index = books.index(of: book) else { return }
+        books[index].title = newTitle
+        books[index].reasonToRead = newReasonToRead
+        saveToPersistentStore()
+    }
+}
