ddcb90144f1e0ed9981767b1db3cb3ed602c4a85
Gerardo Hernandez <gahernandez02@gmail.com>
2019-12-08 22:21:04 -0800

    Completed Storyboard Scenes

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Gerardo Hernandez on 12/8/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var reasonToReadView: UITextView!
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
