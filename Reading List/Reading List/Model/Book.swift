83d8741dee5766f07f57cf1b1066d1bc8da81b61
Aaron Cleveland <toscleveland@gmail.com>
2019-12-17 17:48:00 -0600

    Started Afternoon Project

---
+++ Reading List/Reading List/Model/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Aaron Cleveland on 12/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    let title: String
+    let reasonToRead: String
+    let hasBeenRead: Bool
+    
+    init(<#parameters#>) {
+        <#statements#>
+    }
+}
