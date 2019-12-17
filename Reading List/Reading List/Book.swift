430e2038f76c4fc76479f29c80a5554b6bce10a4
Skye Leahy <skyeleahy@protonmail.com>
2019-12-17 15:45:03 -0800

    first commit, will finish rest of code tonight :)

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,22 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by user162867 on 12/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Codable {
+    var title : String
+    var reasonToRead : String
+    var hasBeenRead : Bool
+    
+    init(title: String, reasonToRead: String, hasBeenRead: Bool = false) {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = hasBeenRead
+    }
+    
+}
