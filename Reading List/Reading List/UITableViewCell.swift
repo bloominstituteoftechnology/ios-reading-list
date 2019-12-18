a51509b481b80c2bbe1fb597b0679526d69e8a19
Stephanie Ballard <user.name>
2019-12-17 20:23:24 -0800

    saving

---
+++ Reading List/Reading List/UITableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  UITableViewCell.swift
+//  Reading List
+//
+//  Created by Stephanie Ballard on 12/17/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class UITableViewCell: BookTableViewCell {
+
+}
