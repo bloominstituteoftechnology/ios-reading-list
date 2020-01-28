afbb51b11bd6d09798079b2c4585c47ff037a992
Nichole Davidson <nicholedavid@gmail.com>
2020-01-28 13:07:32 -0700

    Starting foundation

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,21 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Nichole Davidson on 1/28/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book: Equatable, Codable {
+    let title: String
+    let reasonToRead: String
+    let hasBeenRead: Bool
+    
+    init(title: String, reasonToRead: String, hasBeenRead: Bool) {
+        self.title = title
+        self.reasonToRead = reasonToRead
+        self.hasBeenRead = false
+    }
+}
