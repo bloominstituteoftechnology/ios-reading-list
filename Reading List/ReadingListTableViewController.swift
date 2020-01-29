fe424d34282acfea3c67186fca715527205e012e
Benji <benji@benglobal.us>
2020-01-28 22:30:28 -0600

    Setting up ReadingListTableViewController.swift

--- Reading List/ReadingListTableViewController.swift
+++ Reading List/ReadingListTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ReadingListTableViewController: UITableViewController {
+    
+    let bookController = BookController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +25,24 @@ class ReadingListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        switch section {
+        case 1: return bookController.readBooks.count
+        default: return bookController.unreadBooks.count
+        }
+        
+    }
+    
+    private func bookFor(indexPath: IndexPath) -> Book {
+        switch indexPath.section {
+        case 0: return bookController.readBooks[indexPath.row]
+        default: return bookController.unreadBooks[indexPath.row]
+        }
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
@@ -40,7 +50,7 @@ class ReadingListTableViewController: UITableViewController {
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -50,7 +60,7 @@ class ReadingListTableViewController: UITableViewController {
     }
     */
 
-    /*
+   
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -60,7 +70,7 @@ class ReadingListTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+  
 
     /*
     // Override to support rearranging the table view.
