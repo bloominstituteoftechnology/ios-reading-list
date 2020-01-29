a5515940df33f21ca776b7070b908c6d86e0fdbb
Nichole Davidson <nicholedavid@gmail.com>
2020-01-28 19:37:53 -0700

    Beginning of Part 3

--- Reading List/Reading List/BookTableViewCell.swift
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -9,11 +9,30 @@
 import UIKit
 
 class BookTableViewCell: UITableViewCell {
-
     
-    @IBOutlet weak var readingLabel: UILabel!
+
+    @IBOutlet weak var bookOnListLabel: UILabel!
     @IBOutlet weak var checkedButton: UIButton!
     @IBAction func checkedButtonTapped(_ sender: UIButton) {
+        
+        delegate?.toggleHasBeenRead(for: self)
+    }
+    
+    weak var delegate: BookTableViewCellDelegate?
+    
+    var book: Book?
+    
+    func updateViews() {
+        
+        guard let book = book else { return }
+        
+        bookOnListLabel.text = book.title
+        
+        if book.hasBeenRead == true {
+            checkedButton.imageView?.image = UIImage(named: "checked")
+        } else {
+            checkedButton.imageView?.image = UIImage(named: "unchecked")
+        }
     }
     
     override func awakeFromNib() {
