f8daf064528f210037eb34b35d31059075671657
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-14 01:20:26 -0500

    Set up tableView controller.

---
+++ Reading List/Reading List/View Controller/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Lambda_School_Loaner_259 on 2/14/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
