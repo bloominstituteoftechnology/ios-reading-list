770d34e6d3d2d15441346a2ac06f1f568733544c
Claudia Maciel <clc_80@yahoo.com>
2020-02-14 19:33:16 -0800

    BookTableViewCell complete

--- Reading List/Reading List/View/BookTableViewCell.swift
+++ Reading List/Reading List/View/BookTableViewCell.swift
@@ -15,6 +15,11 @@ class BookTableViewCell: UITableViewCell {
     @IBOutlet var hasBeenReadButton: UIButton!
     
     
+    //MARK: - Properties
+    var book: Book?
+    var delegate: BookTableViewCellDelegate?
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -22,11 +27,23 @@ class BookTableViewCell: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-
     }
+    
+    func updateViews() {
+        bookLabel.text = book?.title
+        var boxImage: UIImage
+        if (book?.hasBeenRead) != nil {
+            boxImage = UIImage(named: "checked")!
+        } else {
+            boxImage = UIImage(named: "unchecked")!
+        }
+
+        hasBeenReadButton.setImage(boxImage, for: .normal)
+    }
+    
+    //MARK: - IBActions
     @IBAction func hasBeenReadButtonTapped(_ sender: Any) {
-        
+        delegate?.toggleHasBeenRead(for: self)
     }
     
 }
