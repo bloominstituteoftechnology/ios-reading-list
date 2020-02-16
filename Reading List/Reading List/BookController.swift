0e545ee962e5b671fa8d90d5a89c98c07464257f
Breanna Young <bree.j.32@gmail.com>
2020-02-15 20:52:23 -0500

    added BookContoller

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,17 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Bree Jeune on 2/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] = []
+    
+    init(books: [Book]){
+        self.books = books
+    }
+}
