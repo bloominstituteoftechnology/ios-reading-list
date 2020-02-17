a21ad89ed6bf7d623c6bc589847fb99d382db0fa
Chad Parker <chad@chadparker.com>
2020-02-16 18:04:07 -0800

    Set up Detail VC

---
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -0,0 +1,23 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Spencer Curtis on 7/31/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonToReadTextField: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func saveButtonTapped(_ sender: Any) {
+
+    }
+}
