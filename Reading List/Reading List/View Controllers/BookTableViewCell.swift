dbd46cab8628e64dbc98aa98b4dd699a3441c0e4
Chad Parker <chad@chadparker.com>
2020-02-16 17:46:43 -0800

    TableView added to storyboard, custom cell, and TableVC, IBOutlets

---
+++ Reading List/Reading List/View Controllers/BookTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Chad Parker on 2/16/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var bookTitleLabel: UILabel!
+    @IBOutlet weak var isReadButton: UIButton!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+    @IBAction func isReadButtonWasTapped(_ sender: UIButton) {
+        
+    }
+}
