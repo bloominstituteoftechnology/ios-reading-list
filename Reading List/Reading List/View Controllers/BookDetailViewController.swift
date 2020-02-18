7fb94cd3a801a8744f1b5b99a29478ed52520ebd
Chad Parker <chad@chadparker.com>
2020-02-18 12:40:30 -0800

    Set up Detail VC

--- Reading List/Reading List/View Controllers/BookDetailViewController.swift
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -13,11 +13,33 @@ class BookDetailViewController: UIViewController {
     @IBOutlet weak var titleTextField: UITextField!
     @IBOutlet weak var reasonToReadTextField: UITextView!
 
+    var bookController: BookController?
+    var book: Book?
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        updateViews()
+    }
+
+    func updateViews() {
+        if let book = book {
+            titleTextField.text = book.title
+            reasonToReadTextField.text = book.reasonToRead
+            title = book.title
+        } else {
+            title = "Add a new book"
+        }
     }
 
     @IBAction func saveButtonTapped(_ sender: Any) {
-
+        guard
+            let newTitle = titleTextField.text,
+            let newReasonToRead = reasonToReadTextField.text else { return }
+        if let book = book {
+            bookController?.update(book, title: newTitle, reasonToRead: newReasonToRead)
+        } else {
+            bookController?.create(title: newTitle, reasonToRead: newReasonToRead)
+        }
     }
 }
