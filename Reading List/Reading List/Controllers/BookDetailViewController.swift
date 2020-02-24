e35c1971c97a6ae45aa00e597b63b0fe578fb47f
Hector L <hectoremil21@hotmail.com>
2020-02-23 22:49:40 -0500

    Create cocoa touch file for detail view. Add outlets for text field and text view. Create action for save button.

---
+++ Reading List/Reading List/Controllers/BookDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Karen Rodriguez on 2/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonsTextView: UITextView!
+    @IBAction func saveButtonTapped(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
