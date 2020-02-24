e8113a032a2f9b2ca5a72dde6cdb00427b7c1a75
Hector L <hectoremil21@hotmail.com>
2020-02-24 12:41:18 -0500

    Create DetailViewDelegate with updateView() func.

---
+++ Reading List/Reading List/Models/DetailViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  DetailViewDelegate.swift
+//  Reading List
+//
+//  Created by Karen Rodriguez on 2/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+protocol DetailViewDelegate {
+    func updateView()
+}
