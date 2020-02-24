777333d2279b871736af228863fcb4ad698c629a
Hector L <hectoremil21@hotmail.com>
2020-02-23 19:42:19 -0500

    Create BookController.swift. Create BookController class.

---
+++ Reading List/Reading List/Controllers/BookController.swift
@@ -0,0 +1,13 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Karen Rodriguez on 2/23/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    var books: [Book] = []
+}
