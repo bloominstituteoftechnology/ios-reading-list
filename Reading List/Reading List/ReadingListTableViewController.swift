0f1634ef47eff3e2908cdceed0bd6fc27caf9444
Mark Gerrior <github@gerrior.com>
2020-02-25 15:38:59 -0800

    ReadingListTableViewController up to step 3

--- Reading List/Reading List/ReadingListTableViewController.swift
+++ Reading List/Reading List/ReadingListTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ReadingListTableViewController: UITableViewController {
 
+    var bookController = BookController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,24 +25,39 @@ class ReadingListTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        
+        // The first section (0) will show the read books
+        // The second section (1) will show the unread books.
+        
+        if section == 0 {
+            return bookController.readBooks.count
+        }
+    
+        // section == 1
+        return bookController.unreadBooks.count
     }
 
-    /*
+    func bookFor(indexPath: IndexPath) -> Book {
+        if indexPath.section == 0 {
+            return bookController.readBooks[indexPath.row]
+        }
+        
+        // section == 1
+        return bookController.unreadBooks[indexPath.row]
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bookCell", for: indexPath) as! BookTableViewCell
 
         // Configure the cell...
-
+        cell.book = bookFor(indexPath: indexPath)
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
