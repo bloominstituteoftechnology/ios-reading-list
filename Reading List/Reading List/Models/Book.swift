4e889573f6e696c1281cd44ac3273f55add12f9c
Hector L <hectoremil21@hotmail.com>
2020-02-25 13:31:11 -0500

    Create wrapper for UIImage

--- Reading List/Reading List/Models/Book.swift
+++ Reading List/Reading List/Models/Book.swift
@@ -7,15 +7,40 @@
 //
 
 import Foundation
+import UIKit
 
 struct Book: Equatable, Codable {
+    static func == (lhs: Book, rhs: Book) -> Bool {
+        return lhs.hasBeenRead == rhs.hasBeenRead && lhs.title == rhs.title && lhs.reasonToRead == rhs.reasonToRead
+    }
+    
     var title: String
     var reasonToRead: String
     var hasBeenRead: Bool
+    var image: Image
     
-    init (title: String, reasonToRead: String, hasBeenRead: Bool = false) {
+    init (title: String, reasonToRead: String, hasBeenRead: Bool = false, image: Image) {
         self.title = title
         self.reasonToRead = reasonToRead
         self.hasBeenRead = hasBeenRead
+        self.image = image
     }
+}
+
+// MARK: - UIImage Codable
+
+struct Image: Codable {
+    let imageData: Data?
+    
+    init(withImage image: UIImage) {
+        self.imageData = UIImagePNGRepresentation(image)
+    }
+    
+    func getImage() -> UIImage? {
+        guard let imageData = self.imageData else { return nil }
+        let image = UIImage(data: imageData)
+        
+        return image
+    }
+    
 }
