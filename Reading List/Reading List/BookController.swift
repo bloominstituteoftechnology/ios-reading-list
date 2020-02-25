339596c05386febe17436ce54718bba9283c4cce
Wyatt Harrell <wyattaharrell@gmail.com>
2020-02-25 15:58:25 -0500

    working on number 7

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,102 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Wyatt Harrell on 2/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+
+class BookController {
+    var books: [Book] = []
+    
+    // CRUD
+    
+    func create(title: String, reasonToRead: String) {
+        let newBook = Book(title: title, reasonToRead: reasonToRead)
+        books.append(newBook)
+        
+        saveToPersistentStore()
+        
+    }
+    
+    func delete(book: Book) {
+        var index = 0
+        for aBook in books {
+            if aBook.title == book.title {
+                books.remove(at: index)
+            }
+            index += 1
+        }
+        
+        saveToPersistentStore()
+    }
+    
+    func updateHasBeenRead(for book: Book) {
+        for i in 0..<books.count {
+            if books[i].title == book.title {
+                if books[i].hasBeenRead {
+                    books[i].hasBeenRead = false
+                } else {
+                    books[i].hasBeenRead = true
+                }
+            }
+        }
+        saveToPersistentStore()
+    }
+    
+    func updateBook(for book: Book) {
+        //
+        saveToPersistentStore()
+    }
+    
+    
+    // MARK: - Persistence
+    
+    var readingListURL: URL? {
+        let fileManager = FileManager()
+        
+        let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        let bookPList = "ReadingList.plist"
+        
+        return documentsDir?.appendingPathComponent(bookPList)
+    }
+    
+    func saveToPersistentStore() {
+        let encoder = PropertyListEncoder()
+        
+        do {
+            let booksData = try encoder.encode(books)
+
+            guard let booksURL = readingListURL else { return }
+            
+            try booksData.write(to: booksURL)
+
+        } catch {
+            print("Unable to save books to plist \(error)")
+        }
+    }
+    
+    func loadFromPersistentStore() {
+        
+        
+        guard let booksURL = readingListURL else { return }
+        let decoder = PropertyListDecoder()
+        
+        do {
+            
+            let booksData = try Data(contentsOf: booksURL)
+            let decodedBooks = try decoder.decode([Book].self, from: booksData)
+            
+            self.books = decodedBooks
+            
+        } catch {
+            print("Error decoding books: \(error)")
+        }
+        
+    }
+    
+}
