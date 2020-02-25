6374cb426db810b687ec68c06813075aaf6f44e5
Nichole Davidson <nicholedavid@gmail.com>
2020-02-25 16:50:11 -0700

    Added objects to vc scenes and constraints

--- Reading List/Reading List/BookTableViewCell.swift
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class BookTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet weak var bookTitle: UILabel!
+    @IBOutlet weak var readCheckBox: UIButton!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    
+    @IBAction func readBoxChecked(_ sender: Any) {
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
