4b95c58fc8e874f34473501cb5ade0aa9b3d4c9f
Mark Gerrior <github@gerrior.com>
2020-02-25 15:01:11 -0800

    Finish Part 2

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Mark Gerrior on 2/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var reasonToReadTextView: UITextView!
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
