ef3836f0049fd7f2ff814eb3dd4610b4eafae4bd
Nichole Davidson <nicholedavid@gmail.com>
2020-02-25 16:38:59 -0700

    Storyboard initial setup

---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Nichole Davidson on 2/25/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
