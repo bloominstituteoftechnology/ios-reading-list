f2cc066f3ab367fcb09bf53b4d9c4a0a5509be2c
Nichole Davidson <nicholedavid@gmail.com>
2020-02-25 21:49:06 -0700

    Setup prepare for segue func and now stuck

--- Reading List/Reading List/View Controllers/ReadingListTableViewController.swift
+++ Reading List/Reading List/View Controllers/ReadingListTableViewController.swift
@@ -79,19 +79,6 @@ class ReadingListTableViewController: UITableViewController, BookTableViewCellDe
         }
     }
     
-
-    
-    
-    
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
     
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -99,31 +86,21 @@ class ReadingListTableViewController: UITableViewController, BookTableViewCellDe
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
+
     
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "AddBookSegue" {
+            if let newBookVC = segue.destination as? BookDetailViewController {
+                newBookVC.delegate = self
+            }
+        
+        } else if segue.identifier == "BookDetailSegue" {
+            if let bookDetailVC = segue.destination as? BookDetailViewController {
+                bookDetailVC.delegate = self
+            }
+        }
     }
-    */
 
 }
