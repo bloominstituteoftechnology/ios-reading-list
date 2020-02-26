84a15b4e97d96afccaafda20359d501425aee1eb
Nichole Davidson <nicholedavid@gmail.com>
2020-02-25 21:38:39 -0700

    Added code to set title, reason and detail info or add book info

--- Reading List/Reading List/View Controllers/BookDetailViewController.swift
+++ Reading List/Reading List/View Controllers/BookDetailViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class BookDetailViewController: UIViewController {
-    
 
     @IBOutlet weak var bookTitleTextField: UITextField!
     @IBOutlet weak var reasonToRead: UITextView!
+    
+    var bookController: BookController?
+    var bookDetailViewController: BookDetailViewController?
+    var book: Book?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,8 +23,26 @@ class BookDetailViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func saveTapped(_ sender: Any) {
+    func updateViews() {
+        if book != nil {
+            self.title = bookTitleTextField.text
+            let reasonString = String(reasonToRead.text)
+            self.reasonToRead.text = reasonString
+            bookDetailViewController?.title = "\(bookTitleTextField.text ?? "Add a new book")"
+        }
     }
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        if book == nil {
+            if let bookController = bookController {
+                bookController.createBook(with: bookTitleTextField.text ?? "Please fill in title", reasonToRead: reasonToRead.text, hasBeenRead: false)
+            }
+        } else {
+            bookController?.updateBookInfo(with: bookTitleTextField.text ?? "Please fill in title", reasonToRead: reasonToRead.text, hasBeenRead: false)
+        }
+    }
+    
+    
     
     /*
     // MARK: - Navigation
