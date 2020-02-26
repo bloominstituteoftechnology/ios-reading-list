7292ceb38744f0f2f0de8653eb1e8a8f1d454fed
Nichole Davidson <nicholedavid@gmail.com>
2020-02-25 17:06:32 -0700

    Added code to switch check box image

--- Reading List/Reading List/Table View Cells/BookTableViewCell.swift
+++ Reading List/Reading List/Table View Cells/BookTableViewCell.swift
@@ -13,13 +13,27 @@ class BookTableViewCell: UITableViewCell {
     @IBOutlet weak var bookTitle: UILabel!
     @IBOutlet weak var readCheckBox: UIButton!
     
+    var book: Book?
+    
+    
+    func updateViews() {
+        
+        guard let book = book else { return }
+        bookTitle.text = book.title
+        
+        if book.hasBeenRead == true {
+            readCheckBox.imageView?.image = UIImage(named: "checked")
+        } else {
+            readCheckBox.imageView?.image = UIImage(named: "unchecked")
+        }
+    }
+    
+    @IBAction func readBoxChecked(_ sender: Any) {
+    }
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-    }
-    
-    
-    @IBAction func readBoxChecked(_ sender: Any) {
     }
     
     override func setSelected(_ selected: Bool, animated: Bool) {
