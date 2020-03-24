ebfbff128e0627f67d7df7482d26291568f4a51d
Cameron Collins <cameroncollins19@yahoo.com>
2020-03-24 16:35:51 -0500

    Setup Actions and Outlets

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,34 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Cameron Collins on 3/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+    @IBOutlet weak var TextField: UITextField!
+    @IBOutlet weak var TextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
