2271c23f80473e77e782c52be8c7b0be1565e1b8
Harmony Radley <harmonyradley99@gmail.com>
2020-03-24 17:22:54 -0400

    Did CRUD methods

--- Reading List/Reading List/Model/BookController.swift
+++ Reading List/Reading List/Model/BookController.swift
@@ -25,6 +25,53 @@ class BookController {
         
     }
     
+    // MARK: - CRUD
+    
+    // Create
+    
+    func createBook(title: String, reasonToRead: String) {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        
+        books.append(book)
+        
+        saveToPersistentStore()
+    }
+    
+    // Delete
+    
+    func delete(book: Book) {
+        guard let index = books.firstIndex(of: book) else { return }
+        
+        books.remove(at: index)
+        
+        saveToPersistentStore()
+    }
+    
+    // Update
+    
+    func updateHasBeenRead(for book: Book) {
+        
+        guard let index = books.firstIndex(of: book) else { return }
+        
+        books[index].hasBeenRead = !books[index].hasBeenRead
+        
+        saveToPersistentStore()
+    }
+    
+    func update(book: Book, title: String, reasonToRead: String) {
+        guard let index = books.firstIndex(of: book) else { return }
+        
+        var updateBook = book
+        
+        updateBook.title = title
+        updateBook.reasonToRead = reasonToRead
+        
+        books.remove(at: index)
+        books.insert(updateBook, at: index)
+    }
+    
+    
+    // MARK: - Persistence
     func saveToPersistentStore() {
         
         do {
