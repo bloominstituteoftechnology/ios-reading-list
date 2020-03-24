2ed385f2e12593b1e7605794af38cd91e01337f8
Nihal Erdal <nihalerdall@gmail.com>
2020-03-24 15:40:04 -0700

    Wiring up - BooktTableViewCell

---
+++ Reading List/Reading List/BookTableViewCellDelegate.swif.swift
@@ -0,0 +1,9 @@
+//
+//  BookTableViewCellDelegate.swif.swift
+//  Reading List
+//
+//  Created by Nihal Erdal on 3/24/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
