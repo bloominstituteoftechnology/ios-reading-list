f8b227a2ab1d71fff274deceb99478666d78a67b
Nonye <nonye@Nonyes-MacBook-Air.local>
2020-04-21 13:47:07 -0700

    Storyboards set up

---
+++ Reading List/BookTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Nonye on 4/21/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var button: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
