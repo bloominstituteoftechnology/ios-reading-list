33cc9285727682fa0a9b765bfd4e34201ff0584c
Ian French <igf2011@hotmail.com>
2020-04-26 18:07:50 -0400

    Initial commit

---
+++ Reading List/Reading List/Model/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Ian French on 4/26/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    var title: String
+    var reasonToRead: String
+    var hasBeenRead: String
+    
+}
