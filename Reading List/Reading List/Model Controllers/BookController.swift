16a2c30fba92a5d12ac55175fcae6b77ae612aa2
Mike Nichols <mike@nixco.net>
2020-05-19 17:25:37 -0500

    Book List Project

---
+++ Reading List/Reading List/Model Controllers/BookController.swift
@@ -0,0 +1,68 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Mike Nichols on 5/19/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+import Foundation
+
+// #1
+class BookController {
+    
+    // #2
+    var books: [Book] = []
+    
+    // #3
+    var readingListURL: URL? {
+        let fileManager = FileManager.default
+        let documentsDir = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
+        let url = documentsDir?.appendingPathComponent("books.plist")
+        
+        return url
+    }
+    
+    // #6
+    func createBook(title: String, reasonToRead: String) {
+        let book = Book(title: title, reasonToRead: reasonToRead)
+        
+        books.append(book)
+        saveToPersistentStore()
+    }
+    
+    // #7
+    
+    // #4
+    func saveToPersistentStore() {
+        do {
+            guard let readingListURL = readingListURL else { return }
+            
+            let encoder = PropertyListEncoder()
+            let booksData = try encoder.encode(books)
+            
+            try booksData.write(to: readingListURL)
+            
+        } catch {
+            print("Error saving books: \(error)")
+            
+        }
+        
+    }
+    
+    // #5
+    func loadFromPersistentStore() {
+        do {
+            guard let readingListURL = readingListURL else { return }
+            
+            let booksData = try Data(contentsOf: readingListURL)
+            let decoder = PropertyListDecoder()
+            let decodedBooks = try decoder.decode([Book].self, from: booksData)
+            
+            self.books = decodedBooks
+            
+        } catch {
+            print("Error loading books from plist: \(error)")
+            
+        }
+    }
+}
