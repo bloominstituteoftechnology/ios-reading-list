7c0b39c5859c1b2c96bc4d0ef06c6c756562c6d0
Waseem Idelbi <idelbiw@gmail.com>
2022-05-19 21:29:26 -0400

    created the BookController file, and in it so far I have the books array, reading list URL, a save method, and a load method for persistence.

---
+++ Reading List/Reading List/BookController.swift
@@ -0,0 +1,48 @@
+//
+//  BookController.swift
+//  Reading List
+//
+//  Created by Waseem Idelbi on 5/19/22.
+//  Copyright © 2022 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+class BookController {
+    
+//MARK: - Properties
+    var books: [Book] = []
+    var readingListURL: URL? {
+        guard let documents = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return nil }
+        return documents.appendingPathComponent("ReadingList.plist")
+    }
+    
+//MARK: - Methods
+    
+    // CRUD Methods
+    func saveToPersistentStore() {
+        let encoder = PropertyListEncoder()
+        guard let url = readingListURL else { return }
+        
+        do {
+            let data = try encoder.encode(books)
+            try data.write(to: url)
+        } catch {
+            print("ERROR! Could not save books, error code: \(error)")
+        }
+        
+    }
+    
+    func loadFromPersistentStore() {
+        do {
+            let decoder = PropertyListDecoder()
+            guard let url = readingListURL else { return }
+            let data = try Data(contentsOf: url)
+            let decodedBooks = try decoder.decode([Book].self, from: data)
+            self.books = decodedBooks
+        } catch {
+            print("ERROR! Could not retrieve books, error code: \(error)")
+        }
+    }
+    
+}
