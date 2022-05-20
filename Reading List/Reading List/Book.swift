39f5530818d2e7d3cb85075433923908cd798208
Waseem Idelbi <idelbiw@gmail.com>
2022-05-19 20:49:42 -0400

    added the model file and created some groups to organize all the project files

---
+++ Reading List/Reading List/Book.swift
@@ -0,0 +1,15 @@
+//
+//  Book.swift
+//  Reading List
+//
+//  Created by Waseem Idelbi on 5/19/22.
+//  Copyright © 2022 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+struct Book {
+    var title:        String
+    var reasonToRead: String
+    var hasBeenRead:  Bool
+}
