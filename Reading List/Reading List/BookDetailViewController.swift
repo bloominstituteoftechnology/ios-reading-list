e6368e752f487d387578a1e3a5f64c3922842fec
Waseem Idelbi <idelbiw@gmail.com>
2022-05-23 21:42:00 -0400

    set up everything in the main storyboard and set up outlets and actions for all the views

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Waseem Idelbi on 5/23/22.
+//  Copyright © 2022 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+
+//MARK: - IBOutlets
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var reasonsToReadTextView: UITextView!
+    
+//MARK: - IBActions
+    @IBAction func SaveButtonTapped(_ sender: Any) {
+        
+    }
+    
+}
---
+++ Reading List/Reading List/BookTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  BookTableViewCell.swift
+//  Reading List
+//
+//  Created by Waseem Idelbi on 5/23/22.
+//  Copyright © 2022 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+
+//MARK: - IBOutlets
+    @IBOutlet weak var BookTitleLabel: UILabel!
+    @IBOutlet weak var hasBeenReadButton: UIButton!
+    
+//MARK: - IBActions
+    @IBAction func hasBeenReadButtonTapped(_ sender: UIButton) {
+        
+    }
+
+}
